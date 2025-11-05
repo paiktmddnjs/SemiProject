aed73f3812bd160e97fc5ad50cc75f0b0906d166
wkddnqls <jwb57863@naver.com>
2025-11-05 14:16:14 +0900

    설명 메시지 입력

---
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+
+
+    @GetMapping("/login") // URL 매핑
+    public String loginPage() {
+        return "Login"; // /WEB-INF/views/Login.jsp 를 찾아서 렌더링
+    }
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "Sign_Up"; // /WEB-INF/views/Sign_Up.jsp
+    }
+
+    @GetMapping("/landing")
+    public String landingPage() {
+        return "Landing_Page"; // /WEB-INF/views/Landing_Page.jsp
+    }
+
+}
